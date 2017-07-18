2d91a2089fe6e507a2f94fd24360ed0e266db2b8
Lin Yicheng <LinYicheng@Azure.local>
2017-07-18 10:09:58 +0800

    yes more minor edits

---
+++ main/puzzl-solver.py
@@ -0,0 +1,8 @@
+#this is basic idea
+# - identify locations of the first two pieces
+# - move to center (e.g. is on side, spin 2x2 until in, if already in position, dont touch)
+# - shift up, spin into position
+# - continue until solved
+
+# #We should create like a Tkinter GUI for this
+#like for entering the current position
