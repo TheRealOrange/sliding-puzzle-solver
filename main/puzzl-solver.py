2b4272cc8601daeb5fce5b9f772b6d0a7eb3d327
TheRealOrange <linyicheng2004@outlook.com>
2017-10-22 13:55:29 +0800

    Update puzzl-solver.py

--- main/puzzl-solver.py
+++ main/puzzl-solver.py
@@ -7,3 +7,36 @@
 # #We should create like a Tkinter GUI for this
 #like for entering the current position
 #change
+print "welcome to a collabaration between an orange and a bat."
+print "..."
+print """
+this is how you input the puzzle:
+if the puzzle is in this state:
++-+-+-+
+|1|2|3|
++-+-+-+
+|4|5|6|
++-+-+-+
+|7|8| |
++-+-+-+
+type 123456780 below.
+"""
+print "..."
+valid=True
+while valid:
+  try:
+    raw_puzzle=int(raw_input("Input your puzzle here!"))
+    print "..."
+    if len(str(raw_puzzle))==9:
+      valid=False
+    else:
+      print " please enter the correct number of digits"
+      print "..."
+  except ValueError:
+    print "..."
+    print "Input the puzzle again."
+    print "..."
+array=(str(raw_puzzle))
+array=" ".join(array)
+array=array.split()
+array=[int(x) for x in array]
